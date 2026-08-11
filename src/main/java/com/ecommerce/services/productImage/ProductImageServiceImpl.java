582e67668c04f9e130d139d62345d68b234065a7
Manuel <andrianarivo24@gmail.com>
2026-08-11 15:29:16 +0300

    chore: save project state before architecture refactoring

--- src/main/java/com/ecommerce/services/productImage/ProductImageServiceImpl.java
+++ src/main/java/com/ecommerce/services/productImage/ProductImageServiceImpl.java
@@ -1,14 +1,26 @@
 package com.ecommerce.services.productImage;
 
 import com.ecommerce.dto.product.ProductRequestDTO;
+import com.ecommerce.dto.productImage.ProductImageRequestDTO;
+import com.ecommerce.dto.productImage.ProductImageResponseDTO;
 import com.ecommerce.entities.Product;
 import com.ecommerce.entities.ProductImage;
+import com.ecommerce.exceptions.ResourceNotFoundException;
+import com.ecommerce.repositories.ProductImageRepository;
+import com.ecommerce.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductImageServiceImpl implements ProductImageService {
+
+    private final ProductImageRepository productImageRepository;
+
+    private final ProductRepository productRepository;
+
     @Override
     public List<ProductImage> mapToEntities(ProductRequestDTO dto, Product product) {
 
@@ -31,5 +43,73 @@ public class ProductImageServiceImpl implements ProductImageService {
                     return image;
                 })
                 .toList();
+    }
+
+    @Override
+    public List<ProductImageResponseDTO> getAll(Long id) {
+        return productImageRepository.findByProductId(id)
+                .stream()
+                .map(this::mapToDTO)
+                .toList();
+    }
+
+    @Override
+    public ProductImageResponseDTO create(Long id, ProductImageRequestDTO dto) {
+
+        Product product = productRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Product not found"));
+
+        ProductImage productImage = ProductImage.builder()
+                .imageUrl(dto.imageUrl())
+                .altText(dto.altText())
+                .displayOrder(dto.displayOrder())
+                .type(dto.type())
+                .product(product)
+                .build();
+
+        ProductImage productImageSaved = productImageRepository.save(productImage);
+
+        return this.mapToDTO(productImageSaved);
+
+    }
+
+    @Override
+    public ProductImageResponseDTO update(Long id, ProductImageRequestDTO dto) {
+
+        ProductImage productImage = productImageRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Product image not found"));
+
+        productImage.setImageUrl(dto.imageUrl());
+        productImage.setAltText(dto.altText());
+        productImage.setDisplayOrder(dto.displayOrder());
+        productImage.setType(dto.type());
+
+        ProductImage updatedProductImage = productImageRepository.save(productImage);
+
+        return this.mapToDTO(updatedProductImage);
+    }
+
+    @Override
+    public void delete(Long id) {
+
+        ProductImage productImage = productImageRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Product image not found"));
+
+        productImageRepository.delete(productImage);
+
+    }
+
+    private ProductImageResponseDTO mapToDTO(ProductImage productImage){
+        return new ProductImageResponseDTO(
+              productImage.getId(),
+              productImage.getImageUrl(),
+              productImage.getAltText(),
+              productImage.getDisplayOrder(),
+              productImage.getType(),
+                productImage.getCreatedAt()
+        );
     }
 }
